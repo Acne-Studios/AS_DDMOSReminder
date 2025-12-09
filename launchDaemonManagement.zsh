dec637c576f7f1ebf02ee63e3b728038cc6e0b48
maxsundellacne <max.sundell@acnestudios.com>
2025-12-09 09:36:45 +0100

    updated acne specific settings

--- launchDaemonManagement.zsh
+++ launchDaemonManagement.zsh
@@ -38,10 +38,10 @@
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local:/usr/local/bin
 
 # Script Version
-scriptVersion="2.1.0b2"
+scriptVersion="2.1.0b2a1"
 
 # Client-side Log
-scriptLog="/var/log/org.churchofjesuschrist.log"
+scriptLog="/private/var/log/Acne_DDMOSReminder.log"
 
 # Minimum Required Version of swiftDialog
 swiftDialogMinimumRequiredVersion="2.5.6.4805"
